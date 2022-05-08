f00566d907624926d9577761bb860c42fec6f4eb
annomao <omaoannkerubo@gmail.com>
2022-05-08 20:52:51 +0300

    add functionality to display listing

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,28 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React ,{ useState,useEffect }from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  let[cardsList, setCardsList] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:6001/listings")
+    .then(res=> res.json())
+    .then(data =>{
+      setCardsList(data)
+    })
+  },[])
+
+  const renderCards = cardsList.map((card)=>{
+    return <ListingCard 
+    key={card.id} 
+    description={card.description} 
+    image={card.image} 
+    location={card.location}/>
+  })
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {renderCards}
       </ul>
     </main>
   );
